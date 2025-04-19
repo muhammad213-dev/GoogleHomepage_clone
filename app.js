0cf6a7ee296e698cfb0657e5b08b31ae09527fa4
muhammad213-dev <145550931+muhammad213-dev@users.noreply.github.com>
2025-04-19 14:05:10 +0500

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ let preSearch = document.getElementById('pre-search')
 let inputField = document.querySelector('.input-field')
 let app = document.getElementById('app')
 let icons = document.querySelector('.icons')
+let signBtn = document.getElementById('sign-btn')
+let overlay = document.getElementById('overlay')
+let signinBox = document.getElementById('sign-in-box')
 inputField.addEventListener('click', ()=> {
     preSearch.classList.toggle('pre-seach-visible')
 })
@@ -11,3 +14,23 @@ const displayIcon = () => {
 }
 
 app.addEventListener('click', displayIcon)
+
+document.addEventListener('click', (e) => {
+    if(!inputField.contains(e.target)) {
+        preSearch.classList.remove('pre-seach-visible')
+    }
+    if(!app.contains(e.target)) {
+        icons.classList.remove('display-icons')
+    }
+})
+
+// make the sign-in box visible on the page
+
+signBtn.addEventListener('click', () => {
+    overlay.style.display = 'block';
+    signinBox.style.display = 'grid'
+})
+overlay.addEventListener('click', ()=> {
+    overlay.style.display = 'none';
+    signinBox.style.display = 'none';
+})
